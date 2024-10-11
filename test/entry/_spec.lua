e67477e9a587f2f6526ff827f71f6fb666c707de
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-11 23:38:53 +0900

    wip

--- test/entry/_spec.lua
+++ test/entry/_spec.lua
@@ -2,9 +2,7 @@ describe("fetch_next", function()
 	it("fetches the next entry", function()
 		vim.cmd("e test/entry/spec.po")
 
-		-- Get the first line
-		local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
-
-		assert.equals("msgstr", first_line)
+		local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+		assert.equals(1, cursor_line)
 	end)
 end)
