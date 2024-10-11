de99df5c66e53183f570eb1798335ed71b2c8e1d
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-11 18:10:11 +0900

    feat: print the statistics on lualine.nvim (#5)

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.0] - 2024-10-11
+
+### Added
+
+- Component for [lualine.nvim](https://github.com/nvim-lualine/lualine.nvim) which prints the current translation status of the PO file
+
+[0.1.0]: https://github.com/toku-sa-n/po.nvim/releases/tag/0.1.0
---
+++ lua/po/statistics.lua
@@ -0,0 +1,27 @@
+local async = require("plenary.async")
+
+local M = {}
+
+---@param filename string The name of the po file.
+---@param statistics_lang string The language of the statistics text.
+---@return string|nil The statistics text of the given po file, or nil on an error.
+local function fetch(filename, statistics_lang)
+	local async_system = async.wrap(vim.system, 3)
+
+	local obj = async_system(
+		{ "msgfmt", "--statistics", "--output-file=/dev/null", filename },
+		{ env = { LANG = statistics_lang }, text = true }
+	)
+
+	if obj.code ~= 0 then
+		return
+	end
+
+	local s, _ = string.gsub(obj.stderr, "^%s*(.-)%s*$", "%1")
+
+	return s
+end
+
+M.fetch = fetch
+
+return M
