e668fc991bf279e0cc1e5cb7a9481e4efd045d41
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-13 23:26:06 +0900

    refactor: let `test` and `lua` have the same tree (#17)

--- test/po/statistics/_spec.lua
+++ test/po/statistics/_spec.lua
@@ -3,7 +3,7 @@ local async = require("plenary.async")
 
 async.tests.describe("fetch", function()
 	async.tests.it("fetches the statistics text of the given po file content.", function()
-		local actual = statistics.fetch("test/statistics/ja.po", "C")
+		local actual = statistics.fetch("test/po/statistics/ja.po", "C")
 
 		assert.equals("0 translated messages.", actual)
 	end)
