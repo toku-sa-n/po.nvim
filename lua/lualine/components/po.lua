f7753a1778ef9441ac93d1405e5bc81f4e05853f
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-13 18:44:07 +0900

    wip

--- lua/lualine/components/po.lua
+++ lua/lualine/components/po.lua
@@ -1,6 +1,13 @@
 local async = require("plenary.async")
 local statistics = require("po.statistics")
 
+---@class lualine.component
+---@field public init fun(self, options: table)
+
+---@class PoComponent : lualine.component
+---@field private super lualine.component
+---@field public init fun(self, options: table)
+---@field public update_status fun(): string
 local m = require("lualine.component"):extend()
 local statusline = ""
 
@@ -8,7 +15,7 @@ function m:init(options)
 	m.super.init(self, options)
 end
 
-function m.update_status(_)
+function m.update_status()
 	local filepath = vim.api.nvim_buf_get_name(0)
 	if filepath == "" then
 		return ""
