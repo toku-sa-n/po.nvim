dcb837c13422602ea2c1c692e98663b9f1731bb3
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-13 18:39:19 +0900

    wip

--- lua/lualine/components/po.lua
+++ lua/lualine/components/po.lua
@@ -1,14 +1,14 @@
 local async = require("plenary.async")
 local statistics = require("po.statistics")
 
-local M = require("lualine.component"):extend()
+local m = require("lualine.component"):extend()
 local statusline = ""
 
-function M:init(options)
-	M.super.init(self, options)
+function m:init(options)
+	m.super.init(self, options)
 end
 
-function M.update_status(_)
+function m.update_status(_)
 	local filepath = vim.api.nvim_buf_get_name(0)
 	if filepath == "" then
 		return ""
@@ -31,4 +31,4 @@ function M.update_status(_)
 	return statusline
 end
 
-return M
+return m
