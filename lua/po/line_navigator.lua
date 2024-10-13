9611ffbc60acca9b9d36261493268235f2b8f019
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-13 19:13:32 +0900

    wip

--- lua/po/line_navigator.lua
+++ lua/po/line_navigator.lua
@@ -5,18 +5,6 @@
 ---@field next fun(self) : string The next line content.
 local LineNavigator = {}
 
----@param current_line_number number The current line number.
----@param get_line fun(line_number : number) : string A function that returns the line content of the given line number.
----@return LineNavigator A new instance of LineNavigator.
-function LineNavigator.new(current_line_number, get_line)
-	local self = setmetatable({}, LineNavigator)
-
-	self.current_line_number = current_line_number
-	self.get_line = get_line
-
-	return self
-end
-
 ---@return string The previous line content.
 function LineNavigator:previous()
 	self.current_line_number = self.current_line_number - 1
@@ -28,3 +16,21 @@ function LineNavigator:next()
 	self.current_line_number = self.current_line_number + 1
 	return self.get_line(self.current_line_number)
 end
+
+local m = {}
+
+---@param current_line_number number The current line number.
+---@param get_line fun(line_number : number) : string A function that returns the line content of the given line number.
+---@return LineNavigator A new instance of LineNavigator.
+local function new(current_line_number, get_line)
+	local self = setmetatable({}, LineNavigator)
+
+	self.current_line_number = current_line_number
+	self.get_line = get_line
+
+	return self
+end
+
+m.new = new
+
+return m
