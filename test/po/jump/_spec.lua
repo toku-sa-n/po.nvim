f7f81010892332d1b108b91e4827b9a3b2493192
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-15 23:02:50 +0900

    wip

--- test/po/jump/_spec.lua
+++
@@ -1,7 +0,0 @@
-describe("next_untranslated_or_fuzzy", function()
-	it("jumps to the closest next untranslated or fuzzy message", function()
-		vim.cmd("e test/fixtures/next_untranslated_or_fuzzy.po")
-
-		assert.are.same({ 1, 1 }, { vim.fn.line("."), vim.fn.col(".") })
-	end)
-end)
