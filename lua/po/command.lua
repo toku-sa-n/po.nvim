3b67d45b2d9d87530fc006cc0bcc4c12ea54b3ee
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-16 18:47:44 +0900

    wip

--- lua/po/command.lua
+++ lua/po/command.lua
@@ -26,7 +26,7 @@ local function po_handler(opts)
 end
 
 function m.setup()
-	vim.api.nvim_create_user_command("Po", po_handler, { nargs = "*" })
+	vim.api.nvim_create_user_command("Po", po_handler, { nargs = "*", description = "Po.nvim commands" })
 end
 
 return m
