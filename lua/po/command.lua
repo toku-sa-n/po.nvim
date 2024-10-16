07ba4ac17b1cc69bd74a3dfe9784fa48daedcf60
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-16 09:42:41 +0900

    wip

--- lua/po/command.lua
+++ lua/po/command.lua
@@ -1,12 +1,42 @@
 local m = {}
 
-local function po_handler(opts)
-	if opts.fargs[1] == "jump" and opts.fargs[2] == "next" then
-		print("Jumping to next")
-	else
-		local msg = "Unknown command: Po " .. table.concat(opts.fargs, " ")
-		vim.api.nvim_err_writeln(msg)
+local subcommands = {
+	{
+		{ "jump", "next" },
+		function()
+			print("Jumping to next")
+		end,
+	},
+}
+
+local function tables_equal(t1, t2)
+	if #t1 ~= #t2 then
+		return false
 	end
+
+	for i = 1, #t1 do
+		if t1[i] ~= t2[i] then
+			return false
+		end
+	end
+
+	return true
+end
+
+---@param opts { fargs: string[] }
+local function po_handler(opts)
+	local args = opts.fargs
+
+	for _, cmd in ipairs(subcommands) do
+		local keys, handler = cmd[1], cmd[2]
+
+		if tables_equal(keys, args) then
+			handler()
+			return
+		end
+	end
+
+	print("Unknown subcommand")
 end
 
 function m.setup()
