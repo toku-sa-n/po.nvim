744b43c7f3cdc130e574b652bc213e262294a707
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-16 18:42:24 +0900

    wip

--- test/po/command/_spec.lua
+++ test/po/command/_spec.lua
@@ -1,15 +1,18 @@
 require("po.command").setup()
 
 describe(":Po jump next", function()
+	before_each(function()
+		vim.cmd("e test/po/command/ja.po")
+
+		vim.fn.cursor(1, 1)
+		assert.are.same({ 1, 1 }, { vim.fn.line("."), vim.fn.col(".") })
+	end)
+
 	it("should exist", function()
 		vim.cmd("Po jump next")
 	end)
 
 	it("jumps to the next untranslated entry", function()
-		vim.cmd("e test/po/command/ja.po")
-
-		assert.are.same({ 1, 1 }, { vim.fn.line("."), vim.fn.col(".") })
-
 		vim.cmd("Po jump next")
 
 		assert.are.same({ 13, 8 }, { vim.fn.line("."), vim.fn.col(".") })
