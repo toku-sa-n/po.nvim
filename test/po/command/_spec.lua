588b41c01b34f5235b51475eaae9ce227f9813e2
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-16 18:17:01 +0900

    wip

--- test/po/command/_spec.lua
+++ test/po/command/_spec.lua
@@ -1,13 +1,5 @@
 require("po.command").setup()
 
-describe("Po command", function()
-	it("should exist", function()
-		local commands = vim.api.nvim_get_commands({})
-
-		assert.not_nil(commands["Po"])
-	end)
-end)
-
 describe(":Po jump next", function()
 	it("should exist", function()
 		vim.cmd("Po jump next")
