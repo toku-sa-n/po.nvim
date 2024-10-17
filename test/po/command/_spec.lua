4e2bd70a88fc6739ed243184aee24c10189c555d
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-17 09:07:10 +0900

    wip

--- test/po/command/_spec.lua
+++ test/po/command/_spec.lua
@@ -7,21 +7,30 @@ end
 describe(":Po jump next", function()
 	before_each(function()
 		vim.cmd("e test/po/command/ja.po")
-
-		vim.fn.cursor(1, 1)
-		assert_cursor_position(1, 1)
 	end)
 
 	it("jumps to the next untranslated entry", function()
+		vim.fn.cursor(1, 1)
+
 		vim.cmd("Po jump next")
 
 		assert_cursor_position(13, 8)
 	end)
 
 	it("jumps to another untranslated entry after the first jump", function()
+		vim.fn.cursor(1, 1)
+
 		vim.cmd("Po jump next")
 		vim.cmd("Po jump next")
 
 		assert_cursor_position(16, 8)
+	end)
+
+	it("jumps to the next untranslated entry with comments", function()
+		vim.fn.cursor(16, 1)
+
+		vim.cmd("Po jump next")
+
+		assert_cursor_position(19, 8)
 	end)
 end)
